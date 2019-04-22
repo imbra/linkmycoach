689274047f3db3185bf7e45b28d3cf692340652e
imbra <milorad.imbra@gmail.com>
2019-04-22 22:03:35 +0200

    program items created

--- src/app/routes/programs/programs/programs.component.ts
+++ src/app/routes/programs/programs/programs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit} from '@angular/core';
+import { Item } from '../../../shared/models/coaching-process-item';
 
 @Component({
   selector: 'app-programs',
@@ -6,13 +7,25 @@ import { Component, OnInit} from '@angular/core';
   styleUrls: ['./programs.component.scss']
 })
 export class ProgramsComponent implements OnInit {
-
-    items = ['Coaching session', 'Enter Goals', 'Good fit conversation', 'Coaching session', 'Book'];
+    items: Item[];
 
     constructor() {
     }
 
     ngOnInit() {
+      this.getItems();
+    }
+
+    getItems(): void {
+      this.items = [
+        {id: '1', title: 'How to get better sessions', type: 'session' },
+        {id: '2', title: 'Basic fit Questions', type: 'file' },
+        {id: '3', title: 'Fill in the Basic Fit Quesions', type: 'task' },
+        {id: '4', title: 'Discuss Basic fit', type: 'session' },
+        {id: '5', title: 'Another day session', type: 'session' },
+        {id: '6', title: 'Follow up task', type: 'task' },
+        {id: '7', title: 'Final session', type: 'session' }
+      ];
     }
 
 }
